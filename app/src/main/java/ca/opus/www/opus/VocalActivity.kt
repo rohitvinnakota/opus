0af30422eb47fad6302044f9c7013126e34178a5
rohitvinnakota <rohitvinnakota@gmail.com>
2017-11-19 00:55:37 -0700

    so fcking tired

--- app/src/main/java/ca/opus/www/opus/VocalActivity.kt
+++ app/src/main/java/ca/opus/www/opus/VocalActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.provider.AlarmClock
 import android.support.v7.app.AppCompatActivity
 import android.media.*
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.util.Log
@@ -15,6 +16,10 @@ import java.io.IOException
 import android.view.View.OnTouchListener
 import android.widget.AdapterView
 import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+
+
 
 
 /**
@@ -24,11 +29,12 @@ class VocalActivity : AppCompatActivity() {
 
     val recorder =  android.media.MediaRecorder()
     var filename = ""
+    private val mStorageRef: StorageReference? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         filename = Environment.getExternalStorageDirectory().getAbsolutePath()
         filename = filename +"/" + Math.random().toString() +  "recorded_audio.3gp"
-
+        val mStorageRef = FirebaseStorage.getInstance().getReference();
         setContentView(R.layout.activity_voice)
         recordButton.setOnClickListener({
             recordVocals()
@@ -60,6 +66,7 @@ class VocalActivity : AppCompatActivity() {
         recorder.release()
 
     }
+
 
 
         }
